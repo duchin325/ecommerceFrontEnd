f8831fc603eda5d2b93397d4d5a5fef0a37cf4a3
Rodrigo Bertero <berterorodrigo@gmail.com>
2023-07-10 11:09:50 -0300

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import styled from 'styled-components'
+import SearchIcon from '@mui/icons-material/Search';
+
+const Container = styled.div`
+    height: 60px;
+`
+const Wrapper = styled.div`
+    padding: 10px 20px;
+    display: flex;
+    justify-content: space-between;
+`
+const Left = styled.div`
+    flex: 1;
+    display: flex;
+    align-items: center;
+`
+const Language = styled.span`
+    font-size: 14px;
+    cursor: pointer;
+`
+const SearchContainer = styled.div`
+    border: 1px solid lightgrey;
+`
+
+const Center = styled.div`
+    flex: 1;
+`
+const Right = styled.div`
+    flex: 1;
+`
+
+const Navbar = () => {
+  return (
+    <Container>
+        <Wrapper>
+            <Left>
+                <Language>EN</Language>
+                <SearchContainer><SearchIcon/></SearchContainer>
+            </Left>
+            <Center>Center</Center>
+            <Right>Right</Right>
+        </Wrapper>
+    </Container>
+  )
+}
+
+export default Navbar
